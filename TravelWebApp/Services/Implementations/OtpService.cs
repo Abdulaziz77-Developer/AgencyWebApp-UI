2a9b732b59d6075ee3a8fdc302fdefcbbdcc3507
Abdulaziz77-Developer <mirboboevabdulaziz5@gmail.com>
2026-02-06 09:48:54 +0500

    integrate admin email notifications in Booking Management

--- TravelWebApp/Services/Implementations/OtpService.cs
+++ TravelWebApp/Services/Implementations/OtpService.cs
@@ -10,18 +10,26 @@ namespace TravelWebApp.Services.Implementations
         {
             _http = http;
         }
-
         public async Task<bool> SendOtpAsync(string email)
         {
-            // Отправляем запрос на твой контроллер
             var response = await _http.PostAsJsonAsync("api/Otp/send-otp", new { Email = email });
             return response.IsSuccessStatusCode;
         }
 
         public async Task<bool> VerifyOtpAsync(string email, string code)
         {
-            // Проверяем код
             var response = await _http.PostAsJsonAsync("api/Otp/verify-otp", new { Email = email, Code = code });
+            return response.IsSuccessStatusCode;
+        }
+        public async Task<bool> SendConfirmationNotificationAsync(string email, int bookingId)
+        {
+            var response = await _http.PostAsync($"api/Email/confirm-notification?email={email}&id={bookingId}", null);
+            return response.IsSuccessStatusCode;
+        }
+
+        public async Task<bool> SendRejectionNotificationAsync(string email, int bookingId)
+        {
+            var response = await _http.PostAsync($"api/Email/reject-notification?email={email}&id={bookingId}", null);
             return response.IsSuccessStatusCode;
         }
     }
--- TravelWebApp/Services/Interfaces/IOtpService.cs
+++ TravelWebApp/Services/Interfaces/IOtpService.cs
@@ -4,5 +4,7 @@
     {
         Task<bool> SendOtpAsync(string email);
         Task<bool> VerifyOtpAsync(string email, string code);
+        Task<bool> SendConfirmationNotificationAsync(string email, int bookingId);
+        Task<bool> SendRejectionNotificationAsync(string email, int bookingId);
     }
 }

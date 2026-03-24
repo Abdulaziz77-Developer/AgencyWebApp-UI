f077c86bb24a6b094c1bb2b1e3101cd4cbd275a5
Abdulaziz77-Developer <mirboboevabdulaziz5@gmail.com>
2026-03-24 14:09:54 +0500

    translate static text and labels to Russian

--- TravelWebApp/Program.cs
+++ TravelWebApp/Program.cs
@@ -1,60 +1,6 @@
-//using Blazored.LocalStorage;
-//using Microsoft.AspNetCore.Authentication.Cookies;
-//using Microsoft.AspNetCore.Components.Authorization;
-//using Microsoft.Extensions.DependencyInjection.Extensions;
-//using TravelWebApp.Auth;
-//using TravelWebApp.Components;
-//using TravelWebApp.Extensions;
-
-//var builder = WebApplication.CreateBuilder(args);
-
-//// Add services to the container.
-//builder.Services.AddRazorComponents().AddInteractiveServerComponents();
-//builder.Services.AddScoped(sp => new HttpClient
-//{
-//    BaseAddress = new Uri("http://localhost:5188/")
-//});
-//builder.Services.AddApplicationServices();
-//builder.Services.AddAuthorizationCore();
-//builder.Services.AddHttpClient();
-//builder.Services.AddBlazoredLocalStorage();
-//builder.Services.AddScoped<AuthService>();
-//builder.Services.AddScoped<AuthState>();
-//builder.Services.AddScoped<AuthMessageHandler>();
-//builder.Services.AddHttpContextAccessor();
-//builder.Services.AddScoped<CustomAuthStateProvider>();
-//builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthStateProvider>();
-//builder.Services.AddAuthorization();
-////builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
-////    .AddCookie();
-
-//builder.Services.AddAuthentication(options => {
-//    options.DefaultScheme = "Cookies";
-//}).AddCookie("Cookies");
-//var app = builder.Build();
-
-//// Configure the HTTP request pipeline.
-//if (!app.Environment.IsDevelopment())
-//{
-//    app.UseExceptionHandler("/Error", createScopeForErrors: true);
-//    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-//    app.UseHsts();
-//}
-
-//app.UseAuthentication();
-//app.UseAuthorization();
-//app.UseHttpsRedirection();
-
-//app.UseStaticFiles();
-//app.UseAntiforgery();
-
-//app.MapRazorComponents<App>().AddInteractiveServerRenderMode();
-
-//app.Run();
-// ... ���� using ...
-
 using Blazored.LocalStorage;
 using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.HttpOverrides; // Добавлено для Docker
 using TravelWebApp.Auth;
 using TravelWebApp.Components;
 using TravelWebApp.Extensions;
@@ -63,28 +9,44 @@ using TravelWebApp.Services.Interfaces;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// 1. ��������� ����������
+// 1. Настройка компонентов
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
-// 2. �������� ��� .NET 8: ��������� ��������� ��������������
 builder.Services.AddCascadingAuthenticationState();
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://192.168.1.81:5180") });
+// 2. Умная настройка HttpClient для Docker
+// Если приложение видит, что оно в контейнере, оно использует имя сервиса, иначе - localhost
+// var backendUrl = Environment.GetEnvironmentVariable("DOTNET_RUNNING_IN_CONTAINER") == "true" 
+//                  ? "http://backend-container:8080" 
+//                  : "http://localhost:8080";
+var backendUrl = "http://192.168.43.237:5180";
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(backendUrl) });
+
+// 3. Настройка заголовков для работы за прокси (Docker)
+builder.Services.Configure<ForwardedHeadersOptions>(options =>
+{
+    options.ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto;
+    options.KnownNetworks.Clear();
+    options.KnownProxies.Clear();
+});
 
 builder.Services.AddApplicationServices();
 builder.Services.AddHttpClient();
 builder.Services.AddBlazoredLocalStorage();
-
 builder.Services.AddScoped<IOtpService, OtpService>();
 
-// 3. ���������� ����������� ���������� (���� ��������� �� ����)
+// 4. Авторизация
 builder.Services.AddScoped<CustomAuthStateProvider>();
 builder.Services.AddScoped<AuthenticationStateProvider>(sp =>
     sp.GetRequiredService<CustomAuthStateProvider>());
 
 builder.Services.AddScoped<AuthService>();
-builder.Services.AddHttpClient<IWeatherService, WeatherService>();
+builder.Services.AddHttpClient<IWeatherService, WeatherService>(client => 
+{
+    client.BaseAddress = new Uri(backendUrl);
+});
+
 builder.Services.AddScoped<AuthState>();
 builder.Services.AddScoped<AuthMessageHandler>();
 builder.Services.AddHttpContextAccessor();
@@ -96,11 +58,22 @@ builder.Services.AddAuthentication(options => {
 
 var app = builder.Build();
 
-// ... ��������� ��� (Middlewares) ...
+// ПОРЯДОК MIDDLEWARE КРИТИЧЕСКИ ВАЖЕН
+app.UseForwardedHeaders(); // Должно быть первым
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    app.UseHsts();
+}
+
+// app.UseHttpsRedirection(); // В Docker часто мешает, если не настроены сертификаты
+
+app.UseStaticFiles();
+
 app.UseAuthentication();
 app.UseAuthorization();
-app.UseStaticFiles();
-app.UseAntiforgery(); // ����������� ����� MapRazorComponents
+app.UseAntiforgery();
 
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
